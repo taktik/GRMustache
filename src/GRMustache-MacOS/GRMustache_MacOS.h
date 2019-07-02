4f1bf0d95d4fd2906fe8fd70f5283e1664127df7
Antoine Duchâteau <ad@taktik.com>
2019-07-02 22:57:39 +0200

    High Sierra compatibility

---
+++ src/GRMustache-MacOS/GRMustache_MacOS.h
@@ -0,0 +1,18 @@
+//
+//  GRMustache_MacOS.h
+//  GRMustache-MacOS
+//
+//  Created by Antoine Duchateau on 22/03/2019.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GRMustache_MacOS.
+FOUNDATION_EXPORT double GRMustache_MacOSVersionNumber;
+
+//! Project version string for GRMustache_MacOS.
+FOUNDATION_EXPORT const unsigned char GRMustache_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRMustache_MacOS/PublicHeader.h>
+
+
